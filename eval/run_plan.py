afd02e5d30d495a5de0906661a9667ccdd46919f
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-03-18 23:30:59 -0400

    make run_plan function

--- eval/run_plan.py
+++ eval/run_plan.py
@@ -77,7 +77,7 @@ def copy_logs(eval_dir, workload_log_dir, timestamp):
             shutil.copy2(src, dst) 
     logging.info(f"Copied logs to {workload_log_dir}")
 
-def run_workload(workload_path, plan_dir, duration, enable_metrics):
+def run_workload(workload_path, plan_name, duration, enable_metrics):
     cmd = [
         'ros2', 'launch', 'mp_eval', 'launch_workload.py',
         f'workload:={workload_path}',
@@ -93,8 +93,8 @@ def run_workload(workload_path, plan_dir, duration, enable_metrics):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     workload_name = os.path.splitext(os.path.basename(workload_path))[0]
     eval_dir = Path(os.path.dirname(__file__))
-    plan_dir_name = os.path.basename(plan_dir)
-    workload_log_dir = eval_dir / 'results' / plan_dir_name / 'logs' / workload_name
+
+    workload_log_dir = eval_dir / 'results' / plan_name / 'logs' / workload_name
     os.makedirs(workload_log_dir, exist_ok=True)
     
     # Create log file for subprocess output
@@ -144,6 +144,39 @@ def run_workload(workload_path, plan_dir, duration, enable_metrics):
         logging.info(f"{green}Workload {workload_path} completed successfully.{reset}")    
     return True
 
+def run_plan(args, plan_dir):
+    plan_name, workloads, runtimes = read_plan(plan_dir)
+    if args.duration == 0.0 and len(runtimes) > len(workloads):
+        logging.error(f"Number of runtimes ({len(runtimes)}) is greater than the number of workloads ({len(workloads)}). Cannot  implement runtime with dynamic loading!")
+        return 1, ()
+    logging.info(f"Found {len(workloads)} workloads to execute")
+
+    num_successful_workloads = 0
+    successful_workloads = []
+    # Explicitly set the output stream to sys.stderr if desired.
+    with tqdm(total=len(workloads), desc="Running workloads", position=0, leave=True, file=sys.stderr) as pbar:
+        for i, workload in enumerate(workloads):
+            if args.duration == 0.0:
+                duration = runtimes[i]
+            else:
+                duration = args.duration
+            success = run_workload(workload, plan_name, duration, args.enable_metrics)
+            if success:
+                num_successful_workloads += 1
+            successful_workloads.append(success)
+            if args.wait > 0 and i < len(workloads) - 1:
+                logging.info(f"Waiting {args.wait} seconds before next workload...")
+                logging.info(f"You can cancel the wait by pressing Ctrl+C")
+                try:
+                    time.sleep(args.wait)
+                except KeyboardInterrupt:
+                    logging.info(f"{red}Wait interrupted by user. Exiting.{reset}")
+                    return 1, ()
+            pbar.update(1)
+
+    return 0, (num_successful_workloads, successful_workloads, workloads)
+    
+
 def main():
     parser = argparse.ArgumentParser(description='Run a plan of workloads')
     parser.add_argument('plan_dir', type=str, help='Directory containing the run.plan file')
@@ -160,43 +193,23 @@ def main():
     logging.info(f"Log file: {log_file}")
 
     try:
-        plan_name, workloads, runtimes = read_plan(plan_dir)
-        if args.duration == 0.0 and len(runtimes) > len(workloads):
-            logging.error(f"Number of runtimes ({len(runtimes)}) is greater than the number of workloads ({len(workloads)}). Cannot  implement runtime with dynamic loading!")
+        exit_code, ret = run_plan(args, plan_dir)
+        if exit_code == 0:
+            num_successful_workloads, successful_workloads, workloads = ret
+            logging.info(f"Plan execution completed. Successful workloads: {num_successful_workloads}/{len(workloads)}")
+            total_time = time.time() - start_time  # Calculate total time
+            hours = int(total_time // 3600)
+            minutes = int((total_time % 3600) // 60)
+            seconds = total_time % 60
+            logging.info(f"Total execution time: {hours:02d}:{minutes:02d}:{seconds:05.2f}")
+        else:
+            logging.error(f"Plan execution failed. Exiting.")
             return 1
-        logging.info(f"Found {len(workloads)} workloads to execute")
-
-        successful_workloads = 0
-        # Explicitly set the output stream to sys.stderr if desired.
-        with tqdm(total=len(workloads), desc="Running workloads", position=0, leave=True, file=sys.stderr) as pbar:
-            for i, workload in enumerate(workloads):
-                if args.duration == 0.0:
-                    duration = runtimes[i]
-                else:
-                    duration = args.duration
-                if run_workload(workload, plan_dir, duration, args.enable_metrics):
-                    successful_workloads += 1
-                if args.wait > 0 and i < len(workloads) - 1:
-                    logging.info(f"Waiting {args.wait} seconds before next workload...")
-                    logging.info(f"You can cancel the wait by pressing Ctrl+C")
-                    try:
-                        time.sleep(args.wait)
-                    except KeyboardInterrupt:
-                        logging.info(f"{red}Wait interrupted by user. Exiting.{reset}")
-                        return 1
-                pbar.update(1)
-
-        logging.info(f"Plan execution completed. Successful workloads: {successful_workloads}/{len(workloads)}")
-        total_time = time.time() - start_time  # Calculate total time
-        hours = int(total_time // 3600)
-        minutes = int((total_time % 3600) // 60)
-        seconds = total_time % 60
-        logging.info(f"Total execution time: {hours:02d}:{minutes:02d}:{seconds:05.2f}")
     except Exception as e:
         logging.error(f"Error executing plan: {str(e)}", exc_info=True)
         return 1
 
-    return 0 if successful_workloads == len(workloads) else 1
+    return 0 if num_successful_workloads == len(workloads) else 1
 
 if __name__ == '__main__':
     exit(main())
